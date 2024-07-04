fbc78a40a19b85d7dd8e11384135833f3823bf27
Matheusrst <matheusriberomatheus@gmail.com>
2024-07-04 11:48:20 -0300

    atualizando view principal

--- financial-balance/routes/web.php
+++ financial-balance/routes/web.php
@@ -13,11 +13,8 @@ use App\Http\Controllers\TransactionController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/dashboard', function () {
+Route::get('/', function () {
     return view('dashboard');
 })->name('dashboard');
 
